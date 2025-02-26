8107541637c9effe75155fe1fb059c92ccb470b4
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2025-02-26 16:41:33 -0500

    Update useSocket.ts

--- src/app/hooks/useSocket.ts
+++ src/app/hooks/useSocket.ts
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react"
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:5000";
+const SOCKET_SERVER_URL = "https://starrail-chatroom-server.onrender.com";
 
 interface MessageProp {
     username: string,
